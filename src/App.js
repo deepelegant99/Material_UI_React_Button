0919d3f895c310cd578eb226c9d08942078fc7c2
Phillip Wong <deepelegant99@gmail.com>
2021-07-22 23:09:25 -0700

    Setup a react project with material UI. Added two buttons, and put them into a button group to get unifority of design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, ButtonGroup } from "@material-ui/core";
+import { Save, Delete } from "@material-ui/icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonGroup variant="contained" size="large">
+          <Button
+            startIcon={<Save />}
+            onClick={() => alert("Save Something")}
+            color="primary"
+            href="#"
+
+            // disabled
+            // style={{
+            //   fontSize: 25
+            // }}
+          >
+            Save
+          </Button>
+          <Button
+            startIcon={<Delete />}
+            onClick={() => alert("Delete Something")}
+            color="secondary"
+            href="#"
+          >
+            Delete
+          </Button>
+        </ButtonGroup>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
