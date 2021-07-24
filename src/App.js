55784bb9e2e046099b0c31f90f08d90d99eb0608
Phillip Wong <deepelegant99@gmail.com>
2021-07-23 20:44:19 -0700

    Added a checkbox, with label

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, ButtonGroup } from "@material-ui/core";
+import {
+  Button,
+  ButtonGroup,
+  Checkbox,
+  FormControlLabel,
+} from "@material-ui/core";
 import { Save, Delete } from "@material-ui/icons";
+import { React, useState } from "react";
 
 function App() {
+  const [checked, setChecked] = useState(true);
+
+  const CheckboxExample = () => {
+    return (
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            onChange={(e) => {
+              setChecked(e.target.checked);
+            }}
+            inputProps={{
+              "arial-label": "secondary checkbox",
+            }}
+            color="primary"
+            // disabled
+            icon={<Save />}
+          />
+        }
+        label="Testing Textbox"
+      />
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
         <ButtonGroup variant="contained" size="large">
           <Button
             startIcon={<Save />}
